6a5f623a8098feb1abf4e34875953c2c9c745cab
MOHD123ANAS <mohammedanas18025@gmail.com>
2025-07-30 22:04:36 +0530

    Update __init__.py

--- bug_fixer/bug_fixer/__init__.py
+++ bug_fixer/bug_fixer/__init__.py
@@ -1,10 +1,10 @@
-def patch():
-    import erpnext.accounts.doctype.payment_entry.payment_entry as pe
+def patch_india_compliance_override():
+    import india_compliance.gst_india.overrides.payment_entry as ic_payment_entry
 
     def patched_get_advance_payment_entries(*args, **kwargs):
         kwargs.pop("default_advance_account", None)
         return []
 
-    pe.get_advance_payment_entries = patched_get_advance_payment_entries
+    ic_payment_entry.get_advance_payment_entries = patched_get_advance_payment_entries
 
-patch()
+patch_india_compliance_override()
